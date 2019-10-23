fe6a7e4ca48a667589c7edb47a238df0ab6d83b8
Gui Ruggiero <guilherme.ruggiero@gmail.com>
2019-10-22 21:24:17 -0400

    hw 7 almost done

--- week7/hw7_part1.py
+++ week7/hw7_part1.py
@@ -23,12 +23,12 @@ def make_request_using_cache(url):
 
     # Data already in cache?
     if unique_ident in CACHE_DICTION:
-        print("Getting cached data...")
+        #print("Getting cached data...")
         return CACHE_DICTION[unique_ident]
 
     # No - fetch data and add it to cache
     else:
-        print("Making a request for new data...")
+        #print("Making a request for new data...")
         resp = requests.get(url, headers={'User-Agent': 'SI_CLASS'})
         CACHE_DICTION[unique_ident] = resp.text
         dumped_json_cache = json.dumps(CACHE_DICTION)
@@ -41,29 +41,64 @@ def make_request_using_cache(url):
 
 def get_umsi_data(page):
     for i in range(0,14):
-        url_complement = "&page=" + i
+        # Creating dictionary
+        umsi_people = {}
+        
+        # Accessing and parsing page of the directory
+        url_complement = "&page=" + str(i)
         #print(url_complement)
         #print(page + url_complement)
         page_text = make_request_using_cache(page + url_complement)
         #print(page_text)
         page_soup = BeautifulSoup(page_text, 'html.parser')
         #print(page_soup)
+        
+        # Finding div where links to contact derails are
+        content_div = page_soup.find(class_="view-content")
+        #print(content_div)
+        links = content_div.find_all('a')
+        #print(links)
 
-        #get name and title
-        #aaa
+        # Going through every UMSI person on the page
+        for link in links:
+            #print(link.text)
+            if link.text == "Contact Details":
 
-        #follow link to get email
-        #aaa
+                # Accessing and parsing profile page
+                details_url_end = link['href']
+                #print(details_url_end)
+                #print("https://www.si.umich.edu" + details_url_end)
+                details_page_text = make_request_using_cache("https://www.si.umich.edu" + details_url_end)
+                #print(details_page_text)
+                details_page_soup = BeautifulSoup(details_page_text, 'html.parser')
+                #print(details_page_soup)
 
-        #write to dictionary
-        #aaa
+                # Finding div where information is and storing information
+                details_div = details_page_soup.find(id="content")
+                name = details_div.find("h2").text
+                #print(name)
+                email = details_div.find("a").text
+                #print(email)
+                title_div = details_div.find(class_="field field-name-field-person-titles field-type-text field-label-hidden")
+                #print(title_div)
+                title = title_div.find(class_="field-item even").text
+                #print(title)
+
+                # Writing to dictionary
+                #print(email, "-", name + ",", title)
+                umsi_people[email] = {'name': name, 'title': title}
+        
+    #print(umsi_people)
+    return umsi_people
 
 #### Execute function, get_umsi_data, here ####
 url = "https://www.si.umich.edu/directory?field_person_firstname_value=&field_person_lastname_value=&rid=All"
-get_umsi_data(url)
+umsi_people = get_umsi_data(url)
+#print(umsi_people)
 
 #### Write out file here #####
-#json = json.dumps(umsi_people)
-#f = open("directory_dict.json", "w")
-#f.write(json)
-#f.close()
+json = json.dumps(umsi_people)
+f = open("directory_dict.json", "w")
+f.write(json)
+f.close()
+print("\nFile 'directory_dict.json' successfully created!\n")
--- week7/hw7_part2.py
+++ week7/hw7_part2.py
@@ -1,22 +1,23 @@
 # 507 Homework 7 Part 2
 # Developed by Gui Ruggiero
 
+import json
+
 count = 0
+
 #### Your Part 2 solution goes here ####
 
 # Reading file and storing content in a dictionary
 dictionary_file = open("directory_dict.json", 'r')
 dictionary_content = dictionary_file.read()
+#print(dictionary_content)
 umsi_people = json.loads(dictionary_content)
+#print(umsi_people)
 dictionary_file.close()
 
-i = 0
 for person in umsi_people:
-    #print(person)
-    #print(person[i])
-    #print(person["title"])
-    #print(person[i]["title"])
-    if person[i]["title"] == "PhD student":
+    #print(umsi_people[person]['title'])
+    if umsi_people[person]['title'] == "PhD student":
         count += 1
 
 #### Your answer output (change the value in the variable, count)####
