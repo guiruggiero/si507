e779b8fbd862f2e5f701ef484e2e5fa3f25bfeb0
Gui Ruggiero <guilherme.ruggiero@gmail.com>
2019-11-13 13:54:25 -0500

    project 3 - part 1 wip, class 10 exercise minor tweaks

---
+++ project3/proj3_choc_edited.py
@@ -0,0 +1,168 @@
+# Developed by Gui Ruggiero
+
+import sqlite3
+import csv
+import json
+
+# Part 1: Read data from CSV and JSON into a new database called choc.db
+DBNAME = "choc.db"
+BARSCSV = "flavors_of_cacao_cleaned.csv"
+COUNTRIESJSON = "countries.json"
+
+# Reading CSV
+csv_file = open(BARSCSV)
+csv_data = csv.reader(csv_file)
+# print(csv_data)
+csv_file.close()
+
+company = []
+bean_name = []
+ref = []
+review = []
+cocoa = []
+location = []
+rating = []
+bean_type = []
+bean_origin = []
+
+for row in csv_data:
+    if row[0] != "Company":
+        company.append(row[0])
+        bean_name.append(row[1])
+        ref.append(row[2])
+        review.append(row[3])
+        cocoa.append(row[4])
+        location.append(row[5])
+        rating.append(row[6])
+        bean_type.append(row[7])
+        bean_origin.append(row[8])
+
+# i = 0
+# print(company[i])
+# print(bean_name[i])
+# print(ref[i])
+# print(review[i])
+# print(cocoa[i])
+# print(location[i])
+# print(rating[i])
+# print(bean_type[i])
+# print(bean_origin[i])
+
+# Reading JSON
+with open(COUNTRIESJSON) as json_file:
+    json_data = json.load(json_file)
+    # print(json_data)
+    # print(json_data[0])
+
+alpha2 = []
+alpha3 = []
+name = []
+region = []
+subregion = []
+population = []
+area = []
+
+for country in json_data:
+    # print(country)
+    alpha2.append(country["alpha2Code"])
+    alpha3.append(country["alpha3Code"])
+    name.append(country["name"])
+    region.append(country["region"])
+    subregion.append(country["subregion"])
+    population.append(country["population"])
+    area.append(country["area"])
+
+# Creating DB
+conn = sqlite3.connect(DBNAME)
+cur = conn.cursor()
+
+# Dropping tables
+statement = "DROP TABLE IF EXISTS 'Bars';"
+cur.execute(statement)
+# print("\nTable 'Bars' dropped (if existed)")
+
+statement = "DROP TABLE IF EXISTS 'Countries';"
+cur.execute(statement)
+# print("Table 'Countries' dropped (if existed)")
+
+conn.commit()
+
+# Creating tables
+statement = """
+    CREATE TABLE 'Bars' (
+        'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
+        'Company' TEXT NOT NULL,
+        'SpecificBeanBarName' TEXT NOT NULL,
+        'REF' TEXT NOT NULL,
+        'ReviewDate' TEXT NOT NULL,
+        'CocoaPercent' REAL NOT NULL,
+        'CompanyLocationId' INTEGER NOT NULL,
+        'Rating' REAL NOT NULL,
+        'BeanType' TEXT,
+        'BroadBeanOriginId' INTEGER NOT NULL,
+    );
+"""
+cur.execute(statement)
+# print("Table 'Bars' created")
+
+# NOT NULL???
+statement = """
+    CREATE TABLE 'Countries' (
+        'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
+        'Alpha2' TEXT,
+        'Alpha3' TEXT,
+        'EnglishName' TEXT,
+        'Region' TEXT,
+        'Subregion' TEXT,
+        'Population' INTEGER,
+        'Area' REAL,
+    );
+"""
+cur.execute(statement)
+# print("Table 'Countries' created")
+
+conn.commit()
+
+# Inserting data into database
+# Table 'Countries'
+i = 0
+for n in name:
+    insertion = (None, alpha2[i], alpha3[i], name[i], region[i], subregion[i], population[i], area[i])
+    statement = 'INSERT INTO "Countries" '
+    statement += 'VALUES (?, ?, ?, ?, ?, ?, ?, ?)'
+    cur.execute(statement, insertion)
+    i += 1
+
+# Table 'Bars' - foreign key???
+#aaa
+
+conn.commit()
+
+
+
+# Part 2: Implement logic to process user commands
+def process_command(command):
+    return []
+
+def load_help_text():
+    with open("help.txt") as f:
+        return f.read()
+
+
+
+# Part 3: Implement interactive prompt. We've started for you!
+def interactive_prompt():
+    help_text = load_help_text()
+    response = ""
+    while response != "exit":
+        response = input("Enter a command: ")
+
+        if response == "help":
+            print(help_text)
+            continue
+
+
+
+# Only runs when this file is run directly
+if __name__=="__main__":
+    interactive_prompt()
--- week10/exercise.py
+++ week10/exercise.py
@@ -10,21 +10,25 @@ statement = '''
     DROP TABLE IF EXISTS 'Airport';
 '''
 cur.execute(statement)
+print("\nTable 'Airport' dropped (if existed)")
 
 statement = '''
     DROP TABLE IF EXISTS 'City';
 '''
 cur.execute(statement)
+print("Table 'City' dropped (if existed)")
 
 statement = '''
     DROP TABLE IF EXISTS 'State';
 '''
 cur.execute(statement)
+print("Table 'State' dropped (if existed)")
 
 statement = '''
     DROP TABLE IF EXISTS 'Country';
 '''
 cur.execute(statement)
+print("Table 'Country' dropped (if existed)")
 
 conn.commit()
 
@@ -36,6 +40,7 @@ statement = '''
     );
 '''
 cur.execute(statement)
+print("Table 'Country' created")
 
 statement = '''
     CREATE TABLE 'State' (
@@ -45,6 +50,7 @@ statement = '''
     );
 '''
 cur.execute(statement)
+print("Table 'State' created")
 
 statement = '''
     CREATE TABLE 'City' (
@@ -55,6 +61,7 @@ statement = '''
     );
 '''
 cur.execute(statement)
+print("Table 'City' created")
 
 statement = '''
     CREATE TABLE 'Airport' (
@@ -68,6 +75,7 @@ statement = '''
     );
 '''
 cur.execute(statement)
+print("Table 'Airport' created")
 
 conn.commit()
 
@@ -83,6 +91,7 @@ country = []
 lat = []
 lgn = []
 traffic = []
+
 for row in csv_data:
     if row[0] != 'iata':
         iata.append(row[0])
@@ -94,8 +103,19 @@ for row in csv_data:
         lgn.append(row[6])
         traffic.append(row[7])
 
-# Inserting data into database
-# countries = [['Mark', 'Newman', 'mwnewman', '4380 NQ'],
+i = 0
+print(iata[i])
+print(airport[i])
+print(city[i])
+print(state[i])
+print(country[i])
+print(lat[i])
+print(lgn[i])
+print(traffic[i])
+
+# Inserting data into database - how to establish relationships?
+
+# instructors = [['Mark', 'Newman', 'mwnewman', '4380 NQ'],
 #             ['Jackie', 'Cohen', 'jczetta', '3333 NQ'],
 #             ['Steven', 'Oney', 'soney', '4366 NQ']]
 # for inst in instructors:
@@ -104,17 +124,17 @@ for row in csv_data:
 #     statement += 'VALUES (?, ?, ?, ?, ?)'
 #     cur.execute(statement, insertion)
 
-#aaa
-
 conn.commit()
 
 # Updating DTW data
 new_taffic = 8500
 airport_code = 'DTW'
+
 update = (new_taffic, airport_code)
 statement = 'UPDATE Airport '
-statement += 'SET traffic=? '
-statement += 'WHERE iata=?'
+statement += 'SET traffic = ? '
+statement += 'WHERE iata = ?'
+
 print(statement)
 cur.execute(statement)
 
@@ -125,6 +145,7 @@ statement = "SELECT Airport.iata, City.city_name, Airport.traffic "
 statement += "FROM Airport JOIN City ON Airport.city_id = City.city_id "
 statement += "JOIN State ON City.state_id = State.state_id "
 statement += "WHERE State.state_abbrev = 'MI'"
+
 cur.execute(statement)
 for row in cur:
     print(row)
